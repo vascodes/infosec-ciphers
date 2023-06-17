1f67f3c9a2016993f5df2d5e62d530de2b60f8f9
vascodes <nikhilvas12@gmail.com>
2023-06-17 22:37:46 +0530

    Update caesarCipher.js to use cutsom mod function.

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,36 +1,43 @@
-let plainText = "HELLO";
+function mod(a, b) {
+	return a >= 0 ? a % b : b - (-a % b);
+}
+
+class CaesarCipher {
+	encrypt(plainText, key) {
+		const encrypted = [];
+
+		for (let i in plainText) {
+			let charCode = plainText.charCodeAt(i),
+				shiftedCharCode = mod(charCode - 65 + key, 26) + 65,
+				encryptedChar = String.fromCharCode(shiftedCharCode);
+
+			encrypted.push(encryptedChar);
+		}
+
+		return encrypted.join("");
+	}
+
+	decrypt(cipherText, key) {
+		const decrypted = [];
+
+		for (let i in cipherText) {
+			let charCode = cipherText.charCodeAt(i),
+				shiftedCharCode = mod(charCode - 65 - key, 26) + 65,
+				decryptedChar = String.fromCharCode(shiftedCharCode);
+
+			decrypted.push(decryptedChar);
+		}
+
+		return decrypted.join("");
+	}
+}
+
+let plainText = "hello".toUpperCase();
 let key = 3;
 
-function encrypt(plainText, key) {
-	const encrypted = [];
+const cs = new CaesarCipher();
+const cipherText = cs.encrypt(plainText, key);
+const decryptedText = cs.decrypt(cipherText, key);
 
-	for (let i in plainText) {
-		let charCode = plainText.charCodeAt(i),
-			shiftedCharCode = ((charCode + key - 65) % 26) + 65,
-			encryptedChar = String.fromCharCode(shiftedCharCode);
-
-		encrypted.push(encryptedChar);
-	}
-
-	return encrypted.join("");
-}
-
-function decrypt(cipherText, key) {
-	const decrypted = [];
-
-	for (let i in cipherText) {
-		let charCode = cipherText.charCodeAt(i),
-			shiftedCharCode = ((charCode - key - 65) % 26) + 65,
-			decryptedChar = String.fromCharCode(shiftedCharCode);
-
-		decrypted.push(decryptedChar);
-	}
-
-	return decrypted.join("");
-}
-
-let cipherText = encrypt(plainText, key);
-let decryptedText = decrypt(cipherText, key);
-
-console.log(cipherText);
-console.log(decryptedText);
+console.log("Cipher:", cipherText);
+console.log("Decipher:", decryptedText);
