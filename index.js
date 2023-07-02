2351580fa4a2a1a640f1d6679d370eadb55980d1
vascodes <nikhilvas12@gmail.com>
2023-07-02 12:34:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ import { CaesarCipher, PlayfairCipher } from "./scripts/ciphers.js";
 const res = document.getElementById("res");
 
 const caesarCipher = new CaesarCipher();
-const playfairCipher = new PlayfairCipher();
-
 let plainText = "hello".toUpperCase();
 let key = 3;
-
 let cipherText = caesarCipher.encrypt(plainText, key);
 let decryptedText = caesarCipher.decrypt(cipherText, key);
 
@@ -17,6 +14,7 @@ res.innerHTML += `Key: ${key} <br />`;
 res.innerHTML += `Caesar Cipher Text: ${cipherText} <br />`;
 res.innerHTML += `Caesar Decrypted Text: ${decryptedText} <br />`;
 
+const playfairCipher = new PlayfairCipher();
 plainText = "INSTRUMENTS";
 key = "MONARCHY";
 cipherText = playfairCipher.encrypt(plainText, key);
