f23219fe649b7ceceef884f426859fdc8cab0063
vascodes <nikhilvas12@gmail.com>
2023-07-13 22:56:23 +0530

    Update playfair cipher UI to show playfair square.

--- playfair.js
+++ playfair.js
@@ -6,11 +6,17 @@ const playfairPlainText = document.getElementById("playfair-plaintext"),
     playfairDecryptKey = document.getElementById("playfair-decrypt-key"),
     btnPlayfairEncrypt = document.getElementById("btn-playfair-encrypt"),
     btnPlayfairDecrypt = document.getElementById("btn-playfair-decrypt"),
+    playfairSquareEncryptionSection = document.getElementById("section-playfair-square-encryption"),
+    playfairSquareDecryptionSection = document.getElementById("section-playfair-square-decryption"),
+    playfairSquareEncryptionContainer = document.getElementById("playfair-square-encryption-container"),
+    playfairSquareDecryptionContainer = document.getElementById("playfair-square-decryption-container"),
     resultSection = document.getElementById("section-result"),
     resultTitle = document.getElementById("result-title"),
     result = document.getElementById("result");
 
 resultSection.style.display = "none";
+playfairSquareEncryptionSection.style.display = "none";
+playfairSquareDecryptionSection.style.display = "none";
 
 btnPlayfairEncrypt.addEventListener("click", () => {
     let plainText = playfairPlainText.value.toUpperCase().replaceAll(" ", ""),
@@ -18,6 +24,24 @@ btnPlayfairEncrypt.addEventListener("click", () => {
 
     const playfairSquare = playfairCipher.playfairSquare;
     let cipherText = playfairCipher.encrypt(plainText, key);
+    
+    playfairSquare.forEach(row => (res.innerHTML += `[${row}] <br />`));
+        
+    let playfairSquareHTML = "";    
+    for (let i = 0; i < playfairSquare.length; i++) {
+        playfairSquareHTML += '<div class="row gx-1 justify-content-center">';
+
+        for (let j = 0; j < playfairSquare.length; j++) {
+            playfairSquareHTML += '<div class="col border border-warning-subtle m-1 py-4">';
+            playfairSquareHTML += playfairSquare[i][j];
+            playfairSquareHTML += '</div>';
+        }
+
+        playfairSquareHTML += '</div>';
+    }
+    
+    playfairSquareEncryptionContainer.innerHTML = playfairSquareHTML;
+    playfairSquareEncryptionSection.style.display = "block";
 
     resultSection.style.display = "block";
     resultTitle.textContent = "Your Ciphertext:";
@@ -29,6 +53,24 @@ btnPlayfairDecrypt.addEventListener("click", () => {
         key = playfairDecryptKey.value;
 
     let decryptedText = playfairCipher.decrypt(cipherText, key);
+
+    const playfairSquare = playfairCipher.playfairSquare;
+
+    let playfairSquareHTML = "";
+    for (let i = 0; i < playfairSquare.length; i++) {
+        playfairSquareHTML += '<div class="row gx-1 justify-content-center">';
+
+        for (let j = 0; j < playfairSquare.length; j++) {
+            playfairSquareHTML += '<div class="col border border-warning-subtle m-1 py-4">';
+            playfairSquareHTML += playfairSquare[i][j];
+            playfairSquareHTML += '</div>';
+        }
+
+        playfairSquareHTML += '</div>';
+    }
+
+    playfairSquareDecryptionContainer.innerHTML = playfairSquareHTML;
+    playfairSquareDecryptionSection.style.display = "block";
 
     resultSection.style.display = "block";
     resultTitle.textContent = "Your Plaintext:";
